79e120e4dadda9dbc3d59db14a7e90d6e80bcbb5
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-29 11:59:46 +0100

    Point 4. Change table color to dark.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 function switchTheme() {
 
-  const body = document.querySelector("body");
-  const nav = document.querySelector("nav");
+  const body = document.getElementsByTagName("body");
+  const nav = document.querySelector(".navbar");
   const cards = document.querySelectorAll(".card");
-  body.classList.add("background-color-dark");
+  body[0].classList.add("background-color-dark");
   nav.classList.add("navbar-dark", "bg-dark");
-
-
 
   for (let i = 0; i < cards.length; i++) {
     cards[i].classList.add("text-white", "bg-dark", "border-light");
   }
+
+  const tables = document.querySelectorAll("table");
+
+  tables.forEach((table)=>{
+    table.setAttribute("class", "table-stripped table table-dark")});
 }
