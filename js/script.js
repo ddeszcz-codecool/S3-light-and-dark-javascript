921d428da1dc48e4e1eafd5d101575f52b7851a2
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-29 12:47:42 +0100

    Point 8 to 14. Reversing settings back to the light theme.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,47 @@
-function switchTheme() {
+const button = document.getElementById("themeSwitch");
+const body = document.getElementsByTagName("body");
+const nav = document.querySelector(".navbar");
+const cards = document.querySelectorAll(".card");
+const tables = document.querySelectorAll("table");
+const footer = document.getElementById("page-footer");
+const header = document.getElementById("page-header");
 
-  const body = document.getElementsByTagName("body");
-  const nav = document.querySelector(".navbar");
-  const cards = document.querySelectorAll(".card");
+let numberOfClicks = 0
+
+button.addEventListener("click", function (){
+  numberOfClicks++;
+  console.log(numberOfClicks);
+  if(numberOfClicks%2===0){
+    switchToLight()
+  }else {
+    switchToDark()
+  }
+});
+
+function switchToLight(){
+  body[0].classList.remove("background-color-dark");
+  nav.classList.remove("navbar-dark", "bg-dark");
+  cards.forEach((card)=>{card.classList.remove("text-white", "bg-dark", "border-light")});
+  tables.forEach((table)=>{
+    table.setAttribute("class","table table-stripped")});
+  footer.classList.remove("text-white", "footer-settings");
+  header.innerHTML = "This is the Light theme";
+  header.classList.remove("text-white");
+  button.innerHTML = "Activate Dark Theme";
+}
+
+function switchToDark(){
   body[0].classList.add("background-color-dark");
   nav.classList.add("navbar-dark", "bg-dark");
-
   for (let i = 0; i < cards.length; i++) {
     cards[i].classList.add("text-white", "bg-dark", "border-light");
   }
-
-  const tables = document.querySelectorAll("table");
-
   tables.forEach((table)=>{
     table.setAttribute("class", "table-stripped table table-dark")});
-
-  const footer = document.getElementById("page-footer");
   footer.classList.add("text-white", "footer-settings");
-
-  const header = document.getElementById("page-header");
   header.innerHTML = "This is the Dark theme";
   header.classList.add("text-white");
-
-  const button = document.getElementById("themeSwitch");
   button.innerHTML = "Activate Light Theme";
-
 }
+
+function switchTheme() {}
