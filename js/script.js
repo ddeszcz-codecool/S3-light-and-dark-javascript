71887d3c66a4d6910f39bee7f521214c08075f88
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-29 11:41:45 +0100

    Point 3. Change cards color to dark.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@ function switchTheme() {
 
   const body = document.querySelector("body");
   const nav = document.querySelector("nav");
+  const cards = document.querySelectorAll(".card");
   body.classList.add("background-color-dark");
-  nav.classList.add("navbar-dark")
-  nav.classList.add("bg-dark")
+  nav.classList.add("navbar-dark", "bg-dark");
 
+
+
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].classList.add("text-white", "bg-dark", "border-light");
+  }
 }
