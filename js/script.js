acbe94972ef50423136b5b20545c18cc03a4d21e
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-29 11:38:09 +0100

    Point 2. Change nav color to dark.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 function switchTheme() {
 
   const body = document.querySelector("body");
+  const nav = document.querySelector("nav");
   body.classList.add("background-color-dark");
+  nav.classList.add("navbar-dark")
+  nav.classList.add("bg-dark")
 
 }
